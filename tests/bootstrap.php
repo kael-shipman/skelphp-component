bc06bd9d36ba78ef958c73eb7c304579cc74c8bd
Kael Shipman <kael.shipman@gmail.com>
2016-12-13 11:53:51 -0600

    Preparing for testing

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,4 +5,8 @@ $path = '.';
 require_once $path.'/vendor/autoload.php';
 require_once $path.'/vendor/skel/interfaces/Interfaces.php';
 
-?>
+require_once $path.'/Component.php';
+require_once $path.'/ComponentCollection.php';
+require_once $path.'/StringTemplate.php';
+require_once $path.'/PowerTemplate.php';
+
